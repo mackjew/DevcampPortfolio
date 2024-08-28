ca61187d88e8a05e51aec526aa6bb234e3ada9b7
MacKenzie Jew <mack.jew@gmail.com>
2024-08-27 20:50:37 -0700

    Updated the file upload forms and refactored placeholder functionality into view. Storage for uploads is currently within the project file itself. I may update this in the future to use AWS storage but not now.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,7 +4,8 @@ class Portfolio < ApplicationRecord
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
     acts_as_list
 
-    include Placeholder
+    # include Placeholder
+    # after_initialize :set_defaults
     validates_presence_of :title, :body, :main_image, :thumb_image
 
     mount_uploader :thumb_image, PortfolioUploader
@@ -21,12 +22,5 @@ class Portfolio < ApplicationRecord
     scope :ruby_on_rails_portfolio_items, -> {
         where(subtitle: "Angular")
     }
-
-    after_initialize :set_defaults
-
-    def set_defaults
-        self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
-        self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
-    end
 end
  
--- db/seeds.rb
+++ db/seeds.rb
@@ -36,27 +36,36 @@ puts "5 skills created"
         title: "Portfolio Title #{portfolio_item}",
         subtitle: "Ruby On Rails",
         body: "this is the body: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        main_image: "https://place-hold.it/600x400",
-        thumb_image: "https://place-hold.it/200x200"
+        #main_image: "https://place-hold.it/600x400", # This is the old way of seeding the main_image. With carrierWave you'll need to use remote_[your_image_attribute_name]_url = "some image URL"
+        remote_main_image_url: "https://place-hold.it/600x400",
+        #thumb_image: "https://place-hold.it/200x200"
+        remote_thumb_image_url: "https://place-hold.it/200x200"
     )
 end
 puts "8 portfolios created"
 
 1.times do |portfolio_item|
-    Portfolio.create!(
-        title: "Portfolio Title #{portfolio_item}",
+    temp =  Portfolio.new(
+        title: "Portfolio Title #{portfolio_item + 1}",
         subtitle: "Angular",
         body: "this is the body: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        main_image: "https://place-hold.it/600x400",
-        thumb_image: "https://place-hold.it/200x200"
+        remote_main_image_url: "https://place-hold.it/600x400",
+        remote_thumb_image_url: "https://place-hold.it/200x200"
     )
+
+    temp.save!
 end
 puts "1 portfolios created"
 
 3.times do |technology_item|
     Technology.create!(
-        name: "Technology #{technology_item}",
+        name: "Technology #{technology_item + 1}",
         portfolio_id: Portfolio.last.id
+    )
+
+    Technology.create!(
+        name: "Technology #{technology_item + 1}",
+        portfolio_id: Portfolio.first.id
     )
 end
 puts "3 technologies created"
