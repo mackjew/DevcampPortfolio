ffbc2f4c359cb90e4bd417727800502c9d4a9df1
MacKenzie Jew <mack.jew@gmail.com>
2024-10-08 21:19:27 -0700

    Adding some js debugging logs to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+console.log("imported @hotwired/turbo-rails");
+
 import "controllers"
+console.log("imported controllers");
+
 import "jquery"
+console.log("imported jquery");
+
 import "cocoon"
+console.log("imported cocoon");
+
 import "gritter"
+console.log("imported gritter");
+
 import "jquery_ujs"
+console.log("imported jquery_ujs");
+
 import "@popperjs/core"
+console.log("imported popperjs/core");
+
 import "bootstrap"
-import "gritter"
+console.log("imported bootstrap");
