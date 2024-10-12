5dfa67090d52176e77cc1bc50e21351ff7f08949
MacKenzie Jew <mack.jew@gmail.com>
2024-10-12 16:03:07 -0700

    cleaning up and removing testing code from application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-
-
 import "@hotwired/turbo-rails"
 console.log("imported @hotwired/turbo-rails");
 
@@ -23,11 +21,5 @@ console.log("imported popperjs/core");
 import "bootstrap"
 console.log("imported bootstrap");
 
-
-var myVar = 0;
-alert(myVar);
-
 import "gritter"
 console.log("imported gritter");
-
-
